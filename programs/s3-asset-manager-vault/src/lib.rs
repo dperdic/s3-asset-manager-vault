d9151dec04c998128d9f957e4e10acc47a047b25
Dujo Perdic <dujo.perdic1@gmail.com>
2024-08-10 21:53:05 +0200

    v1 wip

--- programs/s3-asset-manager-vault/src/lib.rs
+++ programs/s3-asset-manager-vault/src/lib.rs
@@ -1,34 +1,75 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    associated_token::AssociatedToken,
+    // associated_token::AssociatedToken,
     token::{Mint, Token, TokenAccount},
 };
 use std::mem::size_of;
 
 declare_id!("8aFjqAEYZLrHdc2F44mTWJRLV8pECgGiP8kwwQZgVEbs");
 
+const PDA_VAULT_SEED: &[u8; 5] = b"vault";
+
 #[program]
 pub mod s3_asset_manager_vault {
+    use anchor_spl::token::{self, TransferChecked};
+
     use super::*;
 
     pub fn initialize_vault(ctx: Context<InitializeVault>) -> Result<()> {
         let vault: &mut Account<Vault> = &mut ctx.accounts.vault;
 
         vault.manager = ctx.accounts.manager.key();
+        vault.total_deposits = 0;
 
         Ok(())
     }
 
-    pub fn deposit_tokens(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         require!(amount > 0, VaultError::InvalidDepositAmount);
 
-        let vault: &mut Account<Vault> = &mut ctx.accounts.vault;
-        let customer_deposit: &mut Account<CustomerDeposit> = &mut ctx.accounts.customer_deposit;
+        let cpi_accounts: TransferChecked = TransferChecked {
+            mint: ctx.accounts.mint.to_account_info(),
+            from: ctx.accounts.customer_token_account.to_account_info(),
+            to: ctx.accounts.vault_token_account.to_account_info(),
+            authority: ctx.accounts.customer.to_account_info(),
+        };
+
+        let cpi_program: AccountInfo = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx: CpiContext<TransferChecked> = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer_checked(cpi_ctx, amount, ctx.accounts.mint.decimals)?;
 
         Ok(())
     }
 
-    pub fn withdraw_tokens(_ctx: Context<Withdraw>) -> Result<()> {
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        require!(amount > 0, VaultError::InvalidWithdrawAmount);
+
+        let customer_pubkey: Pubkey = ctx.accounts.customer.key();
+
+        let seeds: &[&[u8]; 3] = &[
+            PDA_VAULT_SEED,
+            customer_pubkey.as_ref(),
+            &[ctx.bumps.vault_signer],
+        ];
+
+        let signer_seeds: &[&[&[u8]]; 1] = &[&seeds[..]];
+
+        let cpi_accounts: TransferChecked = TransferChecked {
+            mint: ctx.accounts.mint.to_account_info(),
+            from: ctx.accounts.vault_token_account.to_account_info(),
+            to: ctx.accounts.customer_token_account.to_account_info(),
+            authority: ctx.accounts.vault_signer.to_account_info(),
+        };
+
+        let cpi_program: AccountInfo = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx: CpiContext<TransferChecked> =
+            CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        token::transfer_checked(cpi_ctx, amount, ctx.accounts.mint.decimals)?;
+
         Ok(())
     }
 }
@@ -39,8 +80,6 @@ pub struct InitializeVault<'info> {
         init,
         payer = manager,
         space = size_of::<Vault>() + 8,
-        seeds=[],
-        bump
     )]
     pub vault: Account<'info, Vault>,
 
@@ -55,24 +94,28 @@ pub struct Deposit<'info> {
     #[account(mut)]
     pub vault: Account<'info, Vault>,
 
-    #[account(init_if_needed, payer = customer, space = size_of::<CustomerDeposit>() + 8)]
-    pub customer_deposit: Account<'info, CustomerDeposit>,
-
     #[account(mut)]
     pub customer: Signer<'info>,
 
     #[account(mut)]
-    pub mint_account: Account<'info, Mint>,
-
-    #[account(
-        mut,
-        associated_token::mint = mint_account,
-        associated_token::authority = customer,
-    )]
-    pub user_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
 
     #[account(mut)]
+    pub customer_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = customer,
+        seeds = [PDA_VAULT_SEED, customer.key().as_ref()],
+        bump,
+        token::mint = mint,
+        token::authority = vault_signer
+    )]
     pub vault_token_account: Account<'info, TokenAccount>,
+
+    /// CHECK: This is safe because it's derived from the customer key
+    #[account(seeds = [PDA_VAULT_SEED, customer.key().as_ref()], bump)]
+    pub vault_signer: AccountInfo<'info>,
 
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
@@ -84,17 +127,28 @@ pub struct Withdraw<'info> {
     #[account(mut)]
     pub vault: Account<'info, Vault>,
 
-    #[account(mut, has_one = customer)]
-    pub customer_deposit: Account<'info, CustomerDeposit>,
-
     #[account(mut)]
     pub customer: Signer<'info>,
 
     #[account(mut)]
-    pub customer_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
 
     #[account(mut)]
+    pub customer_token_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds=[PDA_VAULT_SEED, customer.key().as_ref()],
+        bump,
+        token::mint = mint,
+        token::authority = vault_signer
+    )]
     pub vault_token_account: Account<'info, TokenAccount>,
+
+    /// CHECK: This is safe because it's derived from the customer key
+    #[account(seeds = [PDA_VAULT_SEED, customer.key().as_ref()], bump)]
+    pub vault_signer: AccountInfo<'info>,
+
     pub token_program: Program<'info, Token>,
 }
 
@@ -102,12 +156,6 @@ pub struct Withdraw<'info> {
 pub struct Vault {
     pub manager: Pubkey,
     pub total_deposits: u64,
-}
-
-#[account]
-pub struct CustomerDeposit {
-    pub customer: Pubkey,
-    pub amount: u64,
 }
 
 #[error_code]
@@ -125,3 +173,9 @@ pub enum VaultError {
     #[msg("Invalid token account.")]
     InvalidTokenAccount,
 }
+
+// #[account]
+// pub struct CustomerDeposit {
+//     pub customer: Pubkey,
+//     pub amount: u64,
+// }
